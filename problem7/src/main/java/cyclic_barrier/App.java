b9a373135f936f69e32afe03978ea0fa3cb16472
MeowningMaster <meowningmaster@gmail.com>
2022-11-22 09:21:21 +0200

    fix problem 7

--- problem7/src/main/java/cyclic_barrier/App.java
+++ problem7/src/main/java/cyclic_barrier/App.java
@@ -1,10 +1,7 @@
 package cyclic_barrier;
 
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class App {
     public static void main(String[] args) {
@@ -12,12 +9,18 @@ public class App {
         ExecutorService exs = Executors.newFixedThreadPool(n);
         CyclicBarrier barrier = new CyclicBarrier(n, () -> {
             System.out.printf("Cycle\n");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         });
 
         for (int i = 0; i < n; i += 1) {
+            int k = i;
             exs.submit(() -> {
                 while (!Thread.interrupted()) {
-                    System.out.println(i);
+                    System.out.println(k);
                     try {
                         barrier.await();
                     } catch (InterruptedException e) {
